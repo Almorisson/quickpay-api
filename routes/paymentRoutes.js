afd73a093523d83840e50019faaf02ba2191b2fb
Mor NDIAYE <ndiaye_c@etna-alternance.net>
2019-08-11 07:38:30 +0200

    Try to fix the problem of payment save

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { authenticate } = require('../helpers/authHelpers')
-
 const { createPayment, executePayment } = require('../controller/PaymentController')
+const { findCustomerById } = require('../controller/CustomerController')
 
-router.get('/payment', createPayment);
-router.get('/payment/execute', executePayment);
+router.get('/payment/:customerId', createPayment);
+router.get('/payment/execute/:customerId', executePayment);
+
+router.param("customerId", findCustomerById) // any route with :traderId will execute this findCustomerById() first
 
 module.exports = router
