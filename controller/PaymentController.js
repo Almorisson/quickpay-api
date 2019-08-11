aff035af179635bb1db67757144528c92bc2e3c5
Mor NDIAYE <ndiaye_c@etna-alternance.net>
2019-08-11 02:58:44 +0200

    Fixing a crash problem when trying to test paypal checkout endpoint

--- controller/PaymentController.js
+++ controller/PaymentController.js
@@ -8,7 +8,6 @@
 
 const paypal = require('../config/paypal')
 const validationHandler = require('../validations/validationHandler')
-const config = require('../config')
 
 exports.createPayment = async (req, res, next) => {
     try {
@@ -20,8 +19,8 @@ exports.createPayment = async (req, res, next) => {
                 "payment_method": "paypal"
             },
             "redirect_urls": {
-                "return_url": `${config.HOST}/api/v1/transactions/payment/execute`,
-                "cancel_url": `${config.PORT}/api/v1/transactions/payment/cancel`
+                "return_url": `https://quickpay-api.herokuapp.com/api/v1/transactions/payment/execute`,
+                "cancel_url": `https://quickpay-api.herokuapp.com/api/v1/transactions/payment/cancel`
             },
             "transactions": [{
                 "amount": {
