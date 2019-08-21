6bcf8dc47344e53d68997c77fdc19df7b5684e49
Mor NDIAYE <ndiaye_c@etna-alternance.net>
2019-08-21 18:56:53 +0200

    Fix some typos

--- controller/QrcodeController.js
+++ controller/QrcodeController.js
@@ -25,7 +25,7 @@ exports.generateQrCode = async(req, res, next) => {
 
         // Generate QR Code from text
         let __base = null
-        process.env.NODE_ENV === "production" ? __base =  'https://quickpay.herokuapp.com/' : __base = `http://${config.HOST}:${config.PORT}/`;
+        process.env.NODE_ENV === "production" ? __base =  'https://quickpay-api.herokuapp.com/' : __base = `http://${config.HOST}:${config.PORT}/`;
         const qr_png = qr.imageSync(`${__base}api/v1/transactions/createAmountToPay?${iban}&${_id}&${amount}`, { type: 'png'})
         //console.log(qr_png)
 
@@ -43,7 +43,7 @@ exports.generateQrCode = async(req, res, next) => {
         }
         // Generate a random file name
         const name = `${firstName}${lastName}`;
-        let qr_code_file_name = `${nameSociety.replace(' ', '')}_${new Date().getTime()}.png`;
+        let qr_code_file_name = `${nameSociety.replace(' ', '')}_${firstName}_${new Date().getTime()}.png`;
         const file_name = path.join(__dirname, `../public/qr/${qr_code_file_name}`)
         fs.writeFileSync(file_name, qr_png, err => {
             if(err){
