2af10eaf12b458044f8131880a1b802d813026c4
Mor NDIAYE <ndiaye_c@etna-alternance.net>
2019-08-26 21:41:10 +0200

    Modify get to post in PaymentController.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { authenticate } = require('../helpers/authHelpers')
 const { createPayment, executePayment } = require('../controller/PaymentController')
 
-router.get('/payment', createPayment); // TODO: appliquer le middleware authenticate avant déploiment finale
-router.get('/payment/execute', executePayment); // Pas besoin d'apliquer authenticate sur cette route
+router.post('/payment', createPayment); // TODO: appliquer le middleware authenticate avant déploiment finale
+router.get('/payment/execute', executePayment); // Pas besoin d'appliquer authenticate sur cette route
 
 module.exports = router
